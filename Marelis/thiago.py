7d7ab1a3a457167dd9e06f80fbb7a68bac92a092
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-04 22:27:38 -0300

    Add files via upload

---
+++ Marelis/thiago.py
@@ -0,0 +1,9 @@
+#x = int(raw_input("Informe um numero:"))
+#print"O numero informado foi:", x
+
+x = int(input("Informe um numero: "))
+resultado = x * 2
+
+print("O dobro do numero informado foi:", resultado)
+
+
---
+++ Marelis/thiago31/Conversãodecelsiuspfarenheit.py
@@ -0,0 +1,6 @@
+# Conversão de Celsius para Fahrenheit
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C equivale a {fahrenheit:.2f}°F")
