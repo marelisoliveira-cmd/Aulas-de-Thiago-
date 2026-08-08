355da6922982aef4ea86e3c0741c938c6a2c29c0
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-07 21:42:50 -0300

    Add files via upload

---
+++ Thiago/python.py
@@ -0,0 +1,18 @@
+#Digitar Nota 
+
+#Se nota>=90:
+#Responder ("Excelente!")
+#Senão Se nota >= 70:
+#print("Bom, mas pode melhorar.")
+#Senão:
+#print("Precisa estudar mais.")
+
+nota =float(input("Digite a nota:"))
+if nota >= 90:
+    print("Excelente!")
+elif nota >= 70:
+    print("Bom, mas pode melhorar.")
+else:
+    print("Precisa estudar mais.")
+
+
