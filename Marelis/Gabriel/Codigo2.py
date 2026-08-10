256ab966ae8422cc9d775f0ab9be265e746670cd
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-10 20:22:36 -0300

    Add files via upload

---
+++ Marelis/Gabriel/Codigo2.py
@@ -0,0 +1,25 @@
+
+# Conversão de Fahrenheit para Celsius
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5/9
+
+print(f"{fahrenheit}°F equivale a {celsius:.2f}°C")
+
+
+
+#Fórmulas utilizadas:
+
+#Celsius para Fahrenheit:
+#°F = (°C × 9/5) + 32
+#Fahrenheit para Celsius:
+#°C = (°F − 32) × 5/9
+
+#Exemplo de execução:
+
+#Digite a temperatura em Celsius: 25
+#25°C equivale a 77.00°F
+
+#Digite a temperatura em Fahrenheit: 77
+#7°F equivale a 25.00°C
+
