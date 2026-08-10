fcacb9c43eb58669c2a22604abbbde9e1b5ec54c
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-10 20:31:02 -0300

    Delete thiago.py

--- thiago.py
+++
@@ -1,9 +0,0 @@
-#x = int(raw_input("Informe um numero:"))
-#print"O numero informado foi:", x
-
-x = int(input("Informe um numero: "))
-resultado = x * 2
-
-print("O dobro do numero informado foi:", resultado)
-
-
