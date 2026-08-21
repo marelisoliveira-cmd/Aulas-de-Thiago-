89b47d89088879e395da24ce652f889a41aaf3f3
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-20 21:27:04 -0300

    Delete Correção da prova de Thiago directory

--- Correção da prova de Thiago/Exercício1simplificado.py
+++
@@ -1,9 +0,0 @@
-usuario = input("Usuário: ")
-senha = input("Senha: ")
-
-if usuario != "Adm":
-    print("Usuário não encontrado")
-elif senha != "1234":
-    print("Senha incorreta")
-else:
-    print("Login bem-sucedido")
