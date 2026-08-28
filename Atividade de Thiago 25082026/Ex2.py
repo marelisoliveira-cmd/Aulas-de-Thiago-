e7cddb96e1038682df3bf91c224c71f247c5c6fd
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-08-27 21:09:11 -0300

    Update Ex2.py

--- Atividade de Thiago 25082026/Ex2.py
+++ Atividade de Thiago 25082026/Ex2.py
@@ -1,15 +1,33 @@
 #Faça uma programação para somar numero até digitar sair 
 
+#Os espaços em branco (_____) são lugares onde você vai escrever algo. O resto do texto já está pronto, só falta preencher os espaços.
+#O f no Python faz exatamente a mesma coisa! Ele cria um texto com "espaços em branco" (as chaves {}) e você preenche com o valor de uma variável.
 
-soma =0 
+#COM f
+#nome = "João"
+#idade = 20
+#print(f"Olá, meu nome é {nome} e eu tenho {idade} anos.")
 
-numero = float(input("Digite um número ou digite 'sair' para encerrar:"))
+#SEM f
+#nome = "João"
+#idade = 20
+#print("Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos.")
+# ou print("Meu nome é", nome, "e tenho", idade, "anos.")
+
+
+soma = 0
 
 while True:
+    # 1. Recebe como texto (string)
+    numero = input("Digite um número ou digite 'sair' para encerrar: ")
+    
+    # 2. Verifica se o usuário quer sair
     if numero == 'sair':
         break
-    else:
-        soma += numero
-        numero = float(input("Digite um número ou digite 'sair' para encerrar:"))
+    
+    # 3. Se não for 'sair', converte para float e soma
+    soma += float(numero)
+
+print(f"A soma total dos números digitados foi: {soma}")
                
                      
