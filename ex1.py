19f86afc0c624ccc89301af01f88d3ceba066e96
marelisoliveira-cmd <marelis.oliveira@escola.pr.gov.br>
2026-09-01 20:45:48 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,19 @@
+# Nível - Básico 
+
+# 1. Contagem simples Use range para imprimir os números de 0 a 9. (Objetivo: entender o funcionamento básico do range)
+# 2. Contagem personalizada Imprima os números de 5 a 15 usando range (5, 16). (Objetivo: praticar início e fim no range)
+
+# Nível 2 - Intermediário 
+#Números pares Mostre apenas os números pares de 0 a 20 usando range(0,21,2). (Objetivo: usar o parâmetro passo)
+
+
+
+numbers = []
+
+for i in range(0,10):
+    numbers.append(int(input("Informa um número")))
+
+print(numbers)
+
+
+
---
+++ ex2.py
@@ -0,0 +1,6 @@
+numbers = []
+
+for i in range(5,16):
+    numbers.append(int(input("Informa um número")))
+
+print(numbers)
